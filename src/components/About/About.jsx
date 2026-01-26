8b1fcf4555b69096e0f9010726758a526db48ef4
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2026-01-26 10:06:50 +0100

    feat: improve about section content and accessibility

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,45 +1,59 @@
-import React from "react";
+import PropTypes from "prop-types";
 import "./About.css";
 import about_img from "../../assets/about.png";
 import play_icon from "../../assets/play-icon.png";
+
 const About = ({ setPlayState }) => {
+  const openVideo = () => {
+    setPlayState(true);
+  };
+
   return (
     <div className="about">
       <div className="about-left">
-        <img src={about_img} alt="" className="about-img" />
         <img
-          src={play_icon}
-          alt=""
-          className="play-icon"
-          onClick={() => {
-            setPlayState(true);
-          }}
+          src={about_img}
+          alt="Students attending a university lecture"
+          className="about-img"
         />
+
+        <button
+          className="play-button"
+          onClick={openVideo}
+          aria-label="Play campus introduction video"
+        >
+          <img src={play_icon} alt="" className="play-icon" />
+        </button>
       </div>
+
       <div className="about-right">
-        <h3>About University</h3>
-        <h2>Nurturing Tommorows Leaders Today</h2>
+        <h3>About the university</h3>
+        <h2>Nurturing tomorrow’s leaders today</h2>
+
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic et,
-          ratione quasi voluptatibus suscipit porro minima vitae reiciendis
-          quia, ea ipsam totam voluptatem, accusantium odit impedit fugit.
-          Maiores, delectus vero.
+          Our university focuses on practical learning, strong academic
+          foundations, and real-world experience. Students work on modern
+          projects and gain skills employers value.
         </p>
+
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic et,
-          ratione quasi voluptatibus suscipit porro minima vitae reiciendis
-          quia, ea ipsam totam voluptatem, accusantium odit impedit fugit.
-          Maiores, delectus vero.
+          We combine industry partnerships, experienced faculty, and
+          technology-driven teaching methods to prepare students for global
+          careers.
         </p>
+
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic et,
-          ratione quasi voluptatibus suscipit porro minima vitae reiciendis
-          quia, ea ipsam totam voluptatem, accusantium odit impedit fugit.
-          Maiores, delectus vero.
+          Our campus provides modern facilities, collaborative spaces, and a
+          supportive learning environment that helps students grow both
+          professionally and personally.
         </p>
       </div>
     </div>
   );
+};
+
+About.propTypes = {
+  setPlayState: PropTypes.func.isRequired,
 };
 
 export default About;
