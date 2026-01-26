406fa8d5db6076bd73c380ff64d355d0bd31c0d0
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2026-01-26 09:55:16 +0100

    style: add accessible mobile menu button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import menu_icon from "../../assets/menu-icon.png";
@@ -6,59 +6,106 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    });
+    const onScroll = () => {
+      setSticky(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
-    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    setMobileMenu((prev) => !prev);
   };
+
+  const closeMenu = () => {
+    setMobileMenu(false);
+  };
+
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
-      <img src={logo} alt="" className="logo" />
+      <img src={logo} alt="Edusity logo" className="logo" />
+
       <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
-          <Link to="hero" smooth={true} offset={0} duration={500}>
+          <Link to="hero" smooth offset={0} duration={500} onClick={closeMenu}>
             Home
           </Link>
         </li>
+
         <li>
-          <Link to="programs" smooth={true} offset={-260} duration={500}>
+          <Link
+            to="programs"
+            smooth
+            offset={-260}
+            duration={500}
+            onClick={closeMenu}
+          >
             Program
           </Link>
         </li>
+
         <li>
-          <Link to="about" smooth={true} offset={-150} duration={500}>
+          <Link
+            to="about"
+            smooth
+            offset={-150}
+            duration={500}
+            onClick={closeMenu}
+          >
             About us
           </Link>
         </li>
+
         <li>
-          <Link to="campus" smooth={true} offset={-260} duration={500}>
+          <Link
+            to="campus"
+            smooth
+            offset={-260}
+            duration={500}
+            onClick={closeMenu}
+          >
             Campus
           </Link>
         </li>
+
         <li>
-          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+          <Link
+            to="testimonials"
+            smooth
+            offset={-260}
+            duration={500}
+            onClick={closeMenu}
+          >
             Testimonial
           </Link>
         </li>
+
         <li>
           <Link
             to="contact"
-            smooth={true}
+            smooth
             offset={-260}
             duration={500}
             className="btn"
+            onClick={closeMenu}
           >
             Contact Us
           </Link>
         </li>
       </ul>
-      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
+
+      <button
+        className="menu-button"
+        onClick={toggleMenu}
+        aria-label="Toggle navigation menu"
+        aria-expanded={mobileMenu}
+      >
+        <img src={menu_icon} alt="Menu icon" className="menu-icon" />
+      </button>
     </nav>
   );
 };
