1db5acf21a5e33d4c37233d6a950655d6949bd73
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2026-01-26 10:17:50 +0100

    feat: make campus see more action work

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -1,23 +1,57 @@
-import React from "react";
 import "./Campus.css";
-import gallery_1 from "../../assets/gallery-1.png";
-import gallery_2 from "../../assets/gallery-2.png";
-import gallery_3 from "../../assets/gallery-3.png";
-import gallery_4 from "../../assets/gallery-4.png";
-import white_arrow from "../../assets/white-arrow.png";
+import gallery1 from "../../assets/gallery-1.png";
+import gallery2 from "../../assets/gallery-2.png";
+import gallery3 from "../../assets/gallery-3.png";
+import gallery4 from "../../assets/gallery-4.png";
+import whiteArrow from "../../assets/white-arrow.png";
+import { Link } from "react-scroll";
+
+const GALLERY = [
+  {
+    id: 1,
+    image: gallery1,
+    alt: "University campus building exterior",
+  },
+  {
+    id: 2,
+    image: gallery2,
+    alt: "Students walking across campus",
+  },
+  {
+    id: 3,
+    image: gallery3,
+    alt: "Modern university classroom",
+  },
+  {
+    id: 4,
+    image: gallery4,
+    alt: "University library interior",
+  },
+];
+
 const Campus = () => {
   return (
-    <div className="campus">
-      <div className="gallery">
-        <img src={gallery_1} alt="" />
-        <img src={gallery_2} alt="" />
-        <img src={gallery_3} alt="" />
-        <img src={gallery_4} alt="" />
-      </div>
-      <button className="btn dark-btn">
-        See more here <img src={white_arrow} alt="" />
-      </button>
-    </div>
+    <section className="campus">
+      <ul className="gallery">
+        {GALLERY.map((item) => (
+          <li key={item.id}>
+            <img src={item.image} alt={item.alt} />
+          </li>
+        ))}
+      </ul>
+
+      <Link
+        className="btn dark-btn"
+        to="contact"
+        smooth
+        offset={-260}
+        duration={500}
+        aria-label="Scroll to contact section"
+      >
+        See more
+        <img src={whiteArrow} alt="" aria-hidden="true" />
+      </Link>
+    </section>
   );
 };
 
